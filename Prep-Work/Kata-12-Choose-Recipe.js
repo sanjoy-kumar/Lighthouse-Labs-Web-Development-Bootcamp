eea76b87dd8b7a6c949b29dea14ba0961d563e96
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-11-04 20:25:03 -0400

    Kata 12 - Choose Recipe added

---
+++ Prep-Work/Kata-12-Choose-Recipe.js
@@ -0,0 +1,51 @@
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  for (let recipe of recipes) {
+    const a = ~bakeryA.indexOf(recipe.ingredients[0])
+    const b = ~bakeryA.indexOf(recipe.ingredients[1])
+    const c = ~bakeryB.indexOf(recipe.ingredients[0])
+    const d = ~bakeryB.indexOf(recipe.ingredients[1])
+    
+    if ((a || b)  && (c || d)) {
+      return recipe.name;
+    }
+  }
+}
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+    {
+        name: 'Coconut Sponge Cake',
+        ingredients: ['coconut', 'cake base']
+    },
+    {
+        name: 'Persian Cheesecake',
+        ingredients: ['saffron', 'cream cheese']
+    },
+    {
+        name: 'Custard Surprise',
+        ingredients: ['custard', 'ground beef']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+    {
+        name: 'Potato Ganache',
+        ingredients: ['potatoes', 'chocolate']
+    },
+    {
+        name: 'Sweet Fish',
+        ingredients: ['anchovies', 'honey']
+    },
+    {
+        name: "Nima's Famous Dijon Raisins",
+        ingredients: ['dijon mustard', 'raisins']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
